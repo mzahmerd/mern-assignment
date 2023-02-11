4afdb83261dc89b0181d46b4660429d96660931d
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2023-02-11 11:40:31 +0100

    chore: switched frontend to live backend

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:4000/api/auth'
+const API_URL = 'https://mern-mzahmerd.koyeb.app/api/auth'
 
 const register = async (userData)=>{
     const response = await axios
--- frontend/src/features/posts/postService.js
+++ frontend/src/features/posts/postService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {config} from 'dotenv'
 
-const API_URL = 'http://localhost:4000/api/posts'
+const API_URL = 'https://mern-mzahmerd.koyeb.app/api/posts'
 
 const getPosts = async (token)=>{
     const config = {
